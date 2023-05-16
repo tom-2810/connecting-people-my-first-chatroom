8223ef66077d998b92bd5c837e8f1b45170607ff
tom-2810 <tom.poortman@hva.nl>
2023-05-16 18:05:35 +0200

    setup express, ejs, routing

---
+++ index.js
@@ -0,0 +1,46 @@
+import * as path from 'path'
+
+import { Server } from 'socket.io'
+import { createServer } from 'http'
+import express from 'express'
+
+const app = express()
+const http = createServer(app)
+const ioServer = new Server(http)
+const port = process.env.PORT || 8000
+
+// Serveer client-side bestanden
+app.set('view engine', 'ejs')
+app.set('views', './views')
+
+// app.use(express.static(path.resolve('public')))
+
+// Routing met fallback naar 404 pagina
+app.get('/', (req, res) => res.render('index'))
+app.get('*', (req, res) => res.render('404'))
+
+// Start de socket.io server op
+ioServer.on('connection', (client) => {
+  // Log de connectie naar console
+  console.log(`user ${client.id} connected`)
+
+  // Luister naar een message van een gebruiker
+  client.on('message', (message) => {
+    // Log het ontvangen bericht
+    console.log(`user ${client.id} sent message: ${message}`)
+
+    // Verstuur het bericht naar alle clients
+    ioServer.emit('message', message)
+  })
+
+  // Luister naar een disconnect van een gebruiker
+  client.on('disconnect', () => {
+    // Log de disconnect
+    console.log(`user ${client.id} disconnected`)
+  })
+})
+
+// Start een http server op het ingestelde poortnummer en log de url
+http.listen(port, () => {
+  console.log('listening on http://localhost:' + port)
+})
